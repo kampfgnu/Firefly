7d12dc791a10f93a9b5ef0bc34b3bcb949af3bc6
Thomas Heingaertner <t.heingaertner@nousguide.com>
2012-04-27 12:50:14 +0200

    inital commit

---
+++ Firefly/Firefly/FirstViewController.m
@@ -0,0 +1,46 @@
+//
+//  FirstViewController.m
+//  Firefly
+//
+//  Created by kampfgnu on 4/27/12.
+//  Copyright (c) 2012 NOUS. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"First", @"First");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ Firefly/Firefly/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Firefly
+//
+//  Created by kampfgnu on 4/27/12.
+//  Copyright (c) 2012 NOUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
