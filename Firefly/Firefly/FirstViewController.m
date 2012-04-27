f66cd3a807f7ddab8875ce4afc2aa6763109275c
Thomas Heingaertner <t.heingaertner@nousguide.com>
2012-04-27 15:19:26 +0200

    basic xml http requests checked out

--- Firefly/Firefly/FirstViewController.m
+++ Firefly/Firefly/FirstViewController.m
@@ -8,6 +8,10 @@
 
 #import "FirstViewController.h"
 
+#import "AFNetworking.h"
+
+#import <MediaPlayer/MediaPlayer.h>
+
 @interface FirstViewController ()
 
 @end
@@ -24,14 +28,45 @@
     return self;
 }
 							
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    return;
+    
+    
+//    NSURL *baseURL = [NSURL URLWithString:@"http://debian:3689"];
+    NSURL *baseURL = [NSURL URLWithString:@"http://kampfgnu.dyndns.tv:3689"];
+    AFHTTPClient *client = [AFHTTPClient clientWithBaseURL:baseURL];
+    [client setAuthorizationHeaderWithUsername:@"" password:@"mt-daapd_Mongo0815"];
+    client.parameterEncoding = AFJSONParameterEncoding;
+//    client.parameterEncoding = AFFormURLParameterEncoding;
+
+//    NSDictionary *loginParams = [NSDictionary dictionaryWithObject:@"mt-daapd_Mongo0815" forKey:@"password"];
+    NSMutableDictionary *outputParams = [NSMutableDictionary dictionary];
+//    [outputParams setObject:@"1" forKey:@"session-id"];
+//    [outputParams setObject:@"genre" forKey:@"type"];
+    [outputParams setObject:@"dmap.itemid" forKey:@"meta"];
+    [outputParams setObject:@"xml" forKey:@"output"];
+    [client getPath:@"/databases/1/items"
+//    [client getPath:@"/server-info"
+         parameters:outputParams
+            success:^(AFHTTPRequestOperation *operation, id response) {
+                NSString *text = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
+                NSLog(@"Response: %@", text);
+            }
+            failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                NSLog(@"Error");
+                NSLog(@"%@", error);                                    
+            }];
     
     
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+//    MPMoviePlayerViewController *vc = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:@"http://kampfgnu.dyndns.tv:3689/databases/1/items/1000.mp3"]];
+//    [self presentMoviePlayerViewControllerAnimated:vc];
+}
 - (void)viewDidUnload
 {
     [super viewDidUnload];
